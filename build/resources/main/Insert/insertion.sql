f852b959cf989a924124e6e71be2850f4747fc14
mega <megadigimatt@gmail.com>
2021-11-24 00:34:03 +0100

    /build dir pushed on github

---
+++ build/resources/main/Insert/insertion.sql
@@ -0,0 +1,236 @@
+INSERT IGNORE INTO Film(codF, titolo, genere, durata, dataUscita, idSerie)
+	VALUES(00001, 'Star Wars: Episodio VI - Il ritorno dello Jedi',
+				'fantascienza', 134, '1983/05/25'  , NULL) ;
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP) VALUES(24673, 'nicasio',
+						'lucas valley rid', 5858, 94946);
+	-- casa di george lucas
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- produttore esecutivo George Lucas
+	VALUES ('GRGLCS14ES44', 'George', 'Lucas', 'GB98BARC20040156884556',
+				'1944/05/14', '516-527-8719', 24673,1.5, 'Produttore Esecutivo', NULL);
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- sceneggiatore George Lucas
+	VALUES ('GRGLCS14ES44', 'George', 'Lucas', 'GB98BARC20040156884556',
+				'1944/05/14', '516-527-8719', 24673,1.5, 'Sceneggiatore', NULL);
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- sceneggiatore George Lucas
+	VALUES ('GRGLCS14ES44', 'George', 'Lucas', 'GB98BARC20040156884556',
+				'1944/05/14', '516-527-8719', 24673,1.5, 'Produttore', NULL);
+
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- George lucas
+    VALUES (1, 'GRGLCS14ES44');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP) VALUES(38734, 'cardiff',
+						'st angel str', 89, 15234);
+	-- casa regista
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- regista
+	VALUES ('RCHMRQ22IS37', 'Richard', 'Marquand', 'GB98BARC20035361814589',
+				'1937/12/22', '205-802-5689', 38734, 1.5, 'regista', NULL);
+
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- regista
+    VALUES (1, 'RCHMRQ22IS37');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP) VALUES(37065, 'north malvinafurt',
+						'ryan turnpike', 198, 56893);
+	-- Mark Hamill
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- Luke Skywalker
+	VALUES ('MRKHML25IS51', 'Mark', 'Hamill', 'GB56BARC20038438638758',
+				'1951/09/25', '214-989-5138', 37065, NULL, 'attore', NULL);
+
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- luke
+    VALUES (1, 'MRKHML25IS51');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP) VALUES(19639, 'lake khalidside',
+						'heidenreich ways', 338, 81079);
+	-- Harry Ford
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- Ian solo
+	VALUES ('HRSFRD13GS42', 'Harrison', 'Ford', 'GB25BARC20039569987273',
+				'1942-07-13', '205-944-3057', 19639, NULL, 'attore', NULL);
+
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- ian
+    VALUES (1, 'HRSFRD13GS42');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP) VALUES(19447, 'south erictown',
+						'dereck hill', 473, 89559);
+	-- Carrie Fisher
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- Leila Skywalker
+	VALUES ('CRRFSH21NS56','Carrie', 'Fisher', 'GB20BARC20031835638431',
+				'1956-12-21', '231-687-4750', 19447, NULL, 'attore', NULL);
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- leila
+    VALUES (1, 'CRRFSH21NS56');
+
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	--sede della twenty century
+    VALUES(18302, 'san francisco', 'gorgas ave', 1, 94129);
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	--sede territoriale della twenty century
+    VALUES(16395, 'roma', 'Cantalupo in Sabina', 29, 19100);
+
+INSERT IGNORE INTO Enti(P_IVA,nome,codInd)
+    -- ente di distribuizone
+    VALUES (40365320379, "Twenty Century", 16395);
+
+INSERT IGNORE INTO SediTerritoriali(P_IVA, codInd)
+    VALUES (40365320379, 16395);
+
+INSERT IGNORE INTO SerieLetteraria(idSerie, titolo, genere , CF)
+    -- serie letteraria scritta da george lucas
+    VALUES ( 'MRVCMCSW', 'Star Wars-Marvel Comic', 'Sci-fi', 'GRGLCS14ES44');
+
+INSERT IGNORE INTO Sponsor(P_IVA_SPONSOR, nome)
+    -- MC
+    VALUES('21708660770', 'McDonald SPA');
+
+INSERT IGNORE INTO Sponsor(P_IVA_SPONSOR, nome)
+    -- Porsche
+    VALUES('77072550757', 'Porsche');
+
+INSERT IGNORE INTO Sponsor(P_IVA_SPONSOR, nome)
+    -- Bose
+    VALUES('29218600871', 'Bose');
+
+INSERT IGNORE INTO Sponsor(P_IVA_SPONSOR, nome)
+    -- Samsung
+    VALUES('53179880082', 'Samsung Galaxy');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	-- indirizzo michel barnaby
+    VALUES(49429, 'New Henderson', 'Gislason Ridge', 605, 47614);
+
+INSERT IGNORE INTO Finanziatore(P_IVA_FINANZIATORE, nome,codInd ,percentualeGuadagno)
+    -- Finanziatore 1
+    VALUES('31562270996', 'Micheal Barnaby', 49429, 3.2);
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	-- indirizzo starbucks
+    VALUES(15293, 'Vermont', 'Jayden Villages', 3444, 54808 );
+
+INSERT IGNORE INTO Finanziatore(P_IVA_FINANZIATORE, nome,codInd ,percentualeGuadagno)
+    -- La starbucks che investe in starWars !
+    VALUES('40211260761', 'Starbucks',  15293 , 0.19);
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	    -- indirizzo aiuto regista
+        VALUES(17349, 'Arielleport', 'Ashton Tunnel', 645, 48937);
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- Aiuto regista
+	VALUES ('GLBTLY12DS14','Gilbert', 'Taylor', 'GB77BARC20038424775819',
+				'1914-04-12', '582-583-2011', 17349, 3, 'aiuto regista', NULL);
+
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- Aiuto regista
+    VALUES (1, 'GLBTLY12DS14');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	    -- indirizzo capo regista
+        VALUES(19834, 'North Christellemouth', 'Keagan Circles', 6, 35637);
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- Capo regista
+	VALUES ('ANWJRGA24S33','Andrew', 'Jorge', 'GB13BARC20038029125126',
+				'1933-01-24', '342-112-5431', 19834, NULL, 'capo regista', NULL);
+
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- capo regista
+    VALUES (1, 'ANWJRGA24S33');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	    -- indirizzo Stilista
+        VALUES(17439, 'Maine', 'McDermott Manors', 339, 33812);
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- Stilista
+	VALUES ('JNTSTYD27S39','Jonathan', 'Style', 'GB44BARC20031817174534',
+				'1939-04-27', '623-988-4632', 17439, NULL, 'stilista', NULL);
+
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- stilista
+    VALUES (1, 'JNTSTYD27S39');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	    -- indirizzo Operatore fonico
+        VALUES(19900, 'Indiana', 'Brayan Square', 672, 14329);
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- Operatore Fonico
+	VALUES ('KVNMCGE03S18','Kevin', 'McGrail', 'GB44BARC20031817137209',
+				'1918-05-03', '724-829-1163', 19900, NULL, 'operatore', 'fonico');
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- operatore fonico
+    VALUES (1, 'KVNMCGE03S18');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	    -- indirizzo Operatore fotografico
+        VALUES(12346, 'Lake Nyahmouth', 'McGlynn Brooks', 418, 70444);
+
+INSERT IGNORE INTO MembroTroupe(CF, nome, cognome, iban, dataNascita, telefono,
+							codInd, percentualeContributo,
+							ruolo, tipoOperatore)
+	-- Operatore fotografico
+	VALUES ('DVDCLLOE03S18','David', 'Chiello', 'GB44BARC20031817137209',
+				'1918-05-03', '724-829-1163', 12346, NULL, 'operatore', 'fotografico');
+INSERT IGNORE INTO Film_Membro_Troupe(codF, CF)
+    -- operatore fotografico
+    VALUES (1, 'DVDCLLOE03S18');
+
+INSERT IGNORE INTO Indirizzo(codInd, città, via, civico, CAP)
+	    -- indirizzo Magazzino
+        VALUES(19341, 'South Veronafurt', 'Alejandra Ports', 658, 28270);
+
+INSERT IGNORE INTO Magazzino(numMagazzino, codInd)
+    -- magazzino principale
+    VALUES(1,19341);
+
+INSERT IGNORE INTO PosizioneMagazzino(codP, numMagazzino, scaffale, percorso)
+    -- Posizione costume luke skywalker
+VALUES(25588, 1, 2, 'S');
+
+INSERT IGNORE INTO PosizioneMagazzino(codP, numMagazzino, scaffale, percorso)
+    -- Posizione costume leila
+VALUES(25252, 1, 41, 'S');
+
+INSERT IGNORE INTO PosizioneMagazzino(codP, numMagazzino, scaffale, percorso)
+    -- Posizione costume han solo
+VALUES(84425, 1, 7, 'N');
