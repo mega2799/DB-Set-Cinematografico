3820045997b4d0fbec9d368e7bec59d2837b9090
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2021-11-24 18:33:26 +0100

    finally the project can be used on windows too !

--- src/main/java/application/DBConnection.java
+++ src/main/java/application/DBConnection.java
@@ -1,27 +1,35 @@
 package application;
 
 import java.sql.*;
-
+/*
+        Per usarlo in windows su mysqlWorkbench dovete eseguire questa query:
+            ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+            flush privileges;
+        Ed avere importato il file cinema.sql su Workbench.
+*/
 public class DBConnection {
-    static final String DB_URL = "jdbc:mysql://localhost/";
-    static final String USER = "Matte";
-    static final String PASS = "DBPassword";
+    static final String OS = System.getProperty("os.name");
+    static final String DB_URL = "jdbc:mysql://localhost:3306/cinema?characterEncoding=utf8";
+
+    private String user = "root";
+    private String passw = "password";
 
     private Connection connection;
 
 
     public DBConnection(){
+            if(OS.contains("windows")){
+                this.user = "Matte";
+                this.passw = "DBPassword";
+            } else{
+                if(OS.contains("linux")){
+                    this.user = "root";
+                    this.passw = "password";
+                }
+            }
+
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-                /*
-                Connection con=DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/cinema",USER ,PASS);
-                System.out.println("CONNESSO");
-                this.connection = DriverManager.getConnection(DB_URL, USER, PASS);
-                System.out.println("OUT:" + this.connection.toString());
-*/
-                this.connection = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/cinema",USER ,PASS);
+                this.connection = DriverManager.getConnection(DB_URL,user , passw);
                 System.out.println("CONNESSO");
                 System.out.println("OUT:" + this.connection.toString());
 
